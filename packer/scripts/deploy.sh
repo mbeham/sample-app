ea47529f6a9a06f80097edbcc7dc8ae11f4ce50d
Guido Schoening <guido.schoening@jambit.com>
2018-02-08 15:43:39 +0100

    added deploy script

---
+++ packer/scripts/deploy.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+mkdir /var/gs-spring-boot/
+cp /tmp/gs-spring-boot.jar /var/gs-spring-boot/
+cat <<EOF > /etc/systemd/system/gs-spring-boot.service;
+[Unit]
+Description=gs-spring-boot
+After=syslog.target
+
+[Service]
+User=myapp
+ExecStart=/var/gs-spring-boot/gs-spring-boot.jar
+SuccessExitStatus=143
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable gs-spring-boot.service
