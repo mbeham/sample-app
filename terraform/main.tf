2a5f52c3e0f1733d87df2b6b07c4963d690ad74a
Guido Schoening <guido.schoening@jambit.com>
2018-02-12 13:24:13 +0100

    added remote state

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,14 @@ provider "azurerm" {
 
 }
 
+terraform {
+  backend "azurerm" {
+    storage_account_name = "jambitiac"
+    container_name       = "tfstate"
+    key                  = "${ var.user }.terraform.tfstate"
+  }
+}
+
 resource "azurerm_resource_group" "sample_app" {
   name     = "rg_${ var.user }_sample_app"
   location = "${ var.location }"
